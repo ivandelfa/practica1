3499a8f1b8ec82b95ceaea3a26f4ed145d43991b
ivand <ivandelfa@gmail.com>
2021-10-19 12:02:28 +0200

    Completado SaludoActivity y MainActivity, me dispongo a completar el siguiente HoraActivity

--- app/src/main/java/practica1/ivandelfatovar/iesseveroochoa/net/MainActivity.java
+++ app/src/main/java/practica1/ivandelfatovar/iesseveroochoa/net/MainActivity.java
@@ -2,14 +2,42 @@ package practica1.ivandelfatovar.iesseveroochoa.net;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etNombre;
+    private Button btAceptar;
+    private Button btDimeLaHora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Obtenemos una referencia de los controles de la interfaz
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        btAceptar = (Button) findViewById(R.id.btAceptar);
+        btDimeLaHora = (Button) findViewById(R.id.btDimeLaHora);
+        //Declaramos el evento click del botón
+        btAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Creamos el intent
+                Intent intent = new Intent(MainActivity.this, SaludoActivity.class);
+                //Creamos la información a pasar entre las actividades
+                Bundle b = new Bundle();
+                b.putString("NOMBRE", etNombre.getText().toString());
+                //Añadimos la información al intent
+                intent.putExtras(b);
+                //Iniciamos la actividad
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
---
+++ app/src/main/java/practica1/ivandelfatovar/iesseveroochoa/net/SaludoActivity.java
@@ -0,0 +1,28 @@
+package practica1.ivandelfatovar.iesseveroochoa.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SaludoActivity extends AppCompatActivity {
+
+    private TextView tvSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo);
+
+        //Localizamos los controles
+        tvSaludo = (TextView) findViewById(R.id.tvSaludo);
+
+        //Recuperamos la información pasada al intent
+        Bundle bundle = this.getIntent().getExtras();
+
+        //Construimos el mensaje a imprimir en pantalla
+        tvSaludo.setText("Hola " + bundle.getString("NOMBRE"));
+
+
+    }
+}
