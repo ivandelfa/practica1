c3276c6897b31e20cfcca37cf14f65cf65ba8827
ivand <ivandelfa@gmail.com>
2021-10-19 11:24:20 +0200

    Comienzo el proyecto, creo el Textview, el editText y los dos botones. Creo tambien el segundo activity_hora.xml

---
+++ app/src/main/java/practica1/ivandelfatovar/iesseveroochoa/net/HoraActivity.java
@@ -0,0 +1,14 @@
+package practica1.ivandelfatovar.iesseveroochoa.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hora);
+    }
+}
---
+++ app/src/main/java/practica1/ivandelfatovar/iesseveroochoa/net/MainActivity.java
@@ -0,0 +1,15 @@
+package practica1.ivandelfatovar.iesseveroochoa.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
